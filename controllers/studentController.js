3926e05b7ac5550a68a3719b6b3ef4a7c5785f3e
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2024-10-05 03:25:29 +0600

    get single student by students_id

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,5 +1,6 @@
 const database = require("../config/db");
 
+//GET ALL STUDENTS
 const getStudentController = async (req, res) => {
   try {
     const data = await database.query(" SELECT * FROM students");
@@ -10,12 +11,11 @@ const getStudentController = async (req, res) => {
       });
     }
     res.status(200).send({
-        success: true,
-        message: "All Students Records",
-        totalStudents: data[0].length,
-        studentsList: data[0],
-
-    })
+      success: true,
+      message: "All Students Records",
+      totalStudents: data[0].length,
+      studentsList: data[0],
+    });
   } catch (error) {
     console.log(error),
       res.status(500).send({
@@ -26,6 +26,42 @@ const getStudentController = async (req, res) => {
   }
 };
 
+//GET STUDENT BY ID
+const getStudentByIdController = async (req, res) => {
+  try {
+    const studentId = req.params.id;
+    if (!studentId) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalide or Provide wrong Student Id",
+      });
+    }
+
+    const data = await database.query(`SELECT * FROM students WHERE students_id=?`, [
+      studentId,
+    ]);
+    if (!data) {
+      return res.status(400).send({
+        success: false,
+        message: "No record Found",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Get Student Data",
+      studentDetails: data[0],
+    });
+  } catch (error) {
+    console.log(error),
+      res.status(500).send({
+        success: false,
+        message: "Error in Get Student by their Id Api",
+        error,
+      });
+  }
+};
+
 module.exports = {
   getStudentController,
+  getStudentByIdController,
 };
--- routes/studentsRoute.js
+++ routes/studentsRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { getStudentController } = require('../controllers/studentController');
+const { getStudentController, getStudentByIdController } = require('../controllers/studentController');
 
 const router = express.Router();
 
 //GET ALL STUDENT LIST
 router.get('/getAllStudents', getStudentController);
+
+//GET STUDENT BY ID
+router.get('/getStudent/:id', getStudentByIdController)
 
 
 module.exports = router;
